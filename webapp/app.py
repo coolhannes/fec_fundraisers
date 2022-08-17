ee612b1041730b4af506e92429e0c7569b414dd1
frederickfischer <2097398+frederickfischer@users.noreply.github.com>
2022-08-16 23:27:25 -0400

    Pipstuff

---
+++ webapp/app.py
@@ -0,0 +1,29 @@
+import json
+import numpy as np
+import pandas as pd
+import requests
+from requests.adapters import HTTPAdapter, Retry
+from textpack import tp
+from flask import Flask, request
+
+# create the Flask app
+app = Flask(__name__)
+
+# allow both GET and POST requests
+@app.route('/', methods=['GET', 'POST'])
+def form_example():
+    # handle the POST request
+    if request.method == 'POST':
+        language = request.form.get('language')
+        framework = request.form.get('framework')
+        return '''
+                  <h1>The language value is: {}</h1>
+                  <h1>The framework value is: {}</h1>'''.format(language, framework)
+
+    # otherwise handle the GET request
+    return '''
+           <form method="POST">
+               <div><label>Language: <input type="text" name="language"></label></div>
+               <div><label>Framework: <input type="text" name="framework"></label></div>
+               <input type="submit" value="Submit">
+           </form>'''
